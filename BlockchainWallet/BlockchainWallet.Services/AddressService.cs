68b8b72ede6b4d218346f2db28251181307a7447
Spas <Vutovspas@gmail.com>
2018-02-03 12:33:10 +0200

    Services proj

---
+++ BlockchainWallet/BlockchainWallet.Services/AddressService.cs
@@ -0,0 +1,17 @@
+﻿namespace BlockchainWallet.Services
+{
+    using System.Text;
+
+    public class AddressService
+    {
+        public void CreateAddress()
+        {
+            var mnemonic = string.Empty; // todo random words
+            var bytes = Encoding.ASCII.GetBytes(mnemonic);
+
+            // todo create private key -> public key -> address
+
+            // todo return Model with data for showing
+        }
+    }
+}
