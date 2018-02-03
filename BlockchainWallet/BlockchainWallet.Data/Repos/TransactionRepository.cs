3d6653d4b80c4f1f983c11d29d882f331892fa22
AnaYaneva <ana_yaneva@abv.bg>
2018-02-03 20:14:41 +0200

    transactions start

---
+++ BlockchainWallet/BlockchainWallet.Data/Repos/TransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BlockchainWallet.Models;
+
+using System.Text;
+using BlockchainWallet.Models.ViewModels;
+
+namespace BlockchainWallet.Data.Repos
+{
+    class TransactionRepository : Repository<TransactionModel>
+    {
+        public TransactionRepository(BlockchainDbContext context) : base(context)
+        {
+        }
+    }
+}
