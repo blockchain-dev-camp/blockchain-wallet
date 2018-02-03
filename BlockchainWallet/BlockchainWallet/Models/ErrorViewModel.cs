66c93cb512355d221f3744af8fd25c4f20426401
Spas <Vutovspas@gmail.com>
2018-02-03 10:56:48 +0200

    Wallet initial commit

---
+++ BlockchainWallet/BlockchainWallet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlockchainWallet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
